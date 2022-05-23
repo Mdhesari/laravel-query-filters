75e0ac78299717f11e3ab01d12e7843fbc0b60ce
Mohamad Hesari <mdhesari99@gmail.com>
2022-05-23 12:17:38 +0430

    expand scope

---
+++ src/Scopes/ExpandScope.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Mdhesari\LaravelQueryFilters\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class ExpandScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param Builder $query
+     * @param Model $model
+     * @return void
+     */
+    public function apply(Builder $query, Model $model)
+    {
+        $params = request()->all();
+
+        if ( isset($params['expand']) && method_exists($model, 'getExpandRelations') ) {
+            $relations = $model->getExpandRelations();
+
+            $params['expand'] = explode(',', $params['expand']);
+
+            $query->with(
+                array_filter($params['expand'], fn($expand) => in_array($expand, $relations))
+            );
+        }
+    }
+}
