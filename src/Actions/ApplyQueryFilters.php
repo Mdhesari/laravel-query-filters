b8e7a3765480be513a63f20a3801c02ea01c9484
Mohamad Hesari <mdhesari99@gmail.com>
2023-06-17 17:06:58 +0330

    add order_by support

--- src/Actions/ApplyQueryFilters.php
+++ src/Actions/ApplyQueryFilters.php
@@ -3,6 +3,7 @@
 namespace Mdhesari\LaravelQueryFilters\Actions;
 
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
 use Mdhesari\LaravelQueryFilters\LaravelQueryFiltersServiceProvider;
 use Mdhesari\LaravelQueryFilters\Contracts\Expandable;
 
@@ -39,12 +40,40 @@ class ApplyQueryFilters
 
             $params['expand'] = explode(',', $params['expand']);
 
-            $query->with(
-                array_filter($params['expand'], fn($expand) => in_array($expand, $relations))
-            );
+            $query->with($this->getOnlyValidRelations($params['expand'], $relations));
+        }
+
+        if ( isset($data['order_by']) ) {
+            $orderBy = array_filter(explode(',', $data['order_by']), fn($item) => ! empty($item));
+
+            $query->getQuery()->orders = null;
+
+            foreach ($orderBy as $key => $value) {
+                if ( ! in_array($value, $this->getSortings()) ) {
+                    $sort = isset($orderBy[$key + 1]) && in_array($orderBy[$key + 1], $this->getSortings()) ? $orderBy[$key + 1] : 'desc';
+
+                    $value = explode('.', $value);
+
+                    if ( ! isset($value[1]) )
+                        $query->orderBy($value[0], $sort);
+                    else {
+                        $relationTableName = Str::plural($value[0]);
+                        $relationForeignKey = Str::singular($value[0]).'_id';
+
+                        $query->join($value[0], "{$relationTableName}.id", '=', "{$query->getModel()->getTable()}.{$relationForeignKey}");
+
+                        $query->orderBy($value[1], $sort);
+                    }
+                }
+            }
         }
 
         return $query;
+    }
+
+    private function getSortings()
+    {
+        return ['desc', 'asc'];
     }
 
     private function getSearchParams($query): array
@@ -67,5 +96,10 @@ class ApplyQueryFilters
     private function hasUserId($keys): bool
     {
         return in_array('user_id', $keys);
+    }
+
+    private function getOnlyValidRelations(array $expand, $relations)
+    {
+        return array_filter($expand, fn($expand) => in_array($expand, $relations));
     }
 }
